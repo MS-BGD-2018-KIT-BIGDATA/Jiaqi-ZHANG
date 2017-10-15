25eba0391866d8726717ccf9fea7a1b8e411bde6
Jiaqi <jiaqi.zhang@telecom-paristech.fr>
2017-10-15 15:12:27 +0100

    PEP 8

--- Lesson3/exo_cc_lesson_03.py
+++ Lesson3/exo_cc_lesson_03.py
@@ -14,14 +14,14 @@ def getWebDOM(url):
     return res.text
 
 
-def getTownVector(villes, Key, ville , distance_matrix=None):
+def getTownVector(villes, Key, ville, distance_matrix=None):
     gmaps = googlemaps.Client(key=Key)
     if distance_matrix is None:
         distance_matrix = []
     distance_json = gmaps.distance_matrix(ville, villes)
     distance_normalized = json_normalize(
         distance_json, ['rows', 'elements'])['distance']
-   #  distance_matrix[i].append([row['text'] for row in distance_normalized])
+
     distance_matrix += tuple([ville] + [row['text']
                                         for row in distance_normalized])
     # print(Key)
@@ -32,22 +32,24 @@ def getTownVector(villes, Key, ville , distance_matrix=None):
 if __name__ == "__main__":
     start_time = time.time()
 
-    url = "https://fr.wikipedia.org/wiki/Liste_des_communes_de_France_les_plus_peupl%C3%A9es"
+    url = ("https://fr.wikipedia.org/wiki/"
+           "Liste_des_communes_de_France_les_plus_peupl%C3%A9es")
     soup = BeautifulSoup(getWebDOM(url), 'html.parser')
     table = soup.select("table:nth-of-type(1)")[0]
     rows = table.select("tr")
     work_dir = "C:\\Users\\Mauva\\Documents\\Work\\INFMDI721\\Lesson3"
 
-    with open(work_dir+'\\GoogleToken.txt') as fp:
+    with open(work_dir + '\\GoogleToken.txt') as fp:
         Key = fp.read().replace('\n', '')
 
-    villes = [row.select("a")[0].get_text() for row in rows[1:11]]
+    villes = [row.select("a")[0].get_text() for row in rows[1:31]]
 
     p = Pool()
     distance_matrix = list(p.map(partial(getTownVector, villes, Key), villes))
 
     df = pd.DataFrame.from_records(
         distance_matrix, columns=["Villes"] + villes)
-    df.to_csv(work_dir+"\\top_10_town_distance_matrix.csv", index=False, sep=";")
+    df.to_csv(work_dir + "\\top_10_town_distance_matrix.csv",
+              index=False, sep=";")
 
     print("--- %s seconds ---" % (time.time() - start_time))
--- Lesson4/exo_dom_lesson_04.py
+++ Lesson4/exo_dom_lesson_04.py
@@ -7,116 +7,123 @@ from pprint import pprint
 from functools import partial
 import time
 import pandas as pd
+import sys
 
 
 def get_car_list(region, version, vendeur, car_list_url):
+    url = ("https://www.leboncoin.fr/voitures/offres/"
+           "{region}/?q={version}&brd=Renault&mdl=Zoe&f={vendeur}")
     res = requests.get(url.format(region=region,
                                   version=version,
                                   vendeur=vendeur))
 
     car_list_page = BeautifulSoup(res.text, "html.parser")
     car_list_href = car_list_page.select("li[itemscope] > a:nth-of-type(1)")
-    [car_list_url.append(['https:' + x['href'], version, vendeur]) for x in car_list_href]
+    [car_list_url.append(['https:' + x['href'], version, vendeur])
+     for x in car_list_href]
     return car_list_url
+
 
 def get_car_specs(url, version, vendeur):
     car_dict = {}
     res = requests.get(url)
-    car_page = BeautifulSoup(res.text,"html.parser")
-    # releaseDate = car_page.select("div.line:nth-of-type(1) span.value")[0] \
-    #               .get_text()
+    car_page = BeautifulSoup(res.text, "html.parser")
+
     price = car_page.select("span.value")[0].get_text()
     release_date = car_page.select("span.value")[4].get_text()
     kilometrage = car_page.select("span.value")[5].get_text()
     description = car_page.select("p[itemprop='description']")[0].get_text()
 
     try:
-        price = re.search("[0-9]+", price.replace(' ','')).group(0)
-    except:
+        price = re.search("[0-9]+", price.replace(' ', '')).group(0)
+    except AttributeError:
         price = ""
 
     try:
         release_date = re.search("[0-9]{4}", release_date).group(0)
-    except:
+    except AttributeError:
         release_date = ""
 
     try:
-        kilometrage = re.search("[0-9]+", kilometrage.replace(' ','')).group(0)
-    except:
+        kilometrage = re.search(
+            "[0-9]+", kilometrage.replace(' ', '')).group(0)
+    except AttributeError:
         kilometrage = ""
 
-    desc_parsed = description.replace(' ','').replace('.','')
+    desc_parsed = description.replace(' ', '').replace('.', '')
 
     try:
-        tel_fixe = re.search("((\+|00)33\s?|0)[1](\s?\d{2}){4}", desc_parsed).group(0)
-    except:
+        tel_fixe = re.search(
+            "((\+|00)33\s?|0)[1](\s?\d{2}){4}", desc_parsed).group(0)
+    except AttributeError:
         tel_fixe = ""
 
     try:
-        tel_port = re.search("((\+|00)33\s?|0)[67](\s?\d{2}){4}", desc_parsed).group(0)
-    except:
+        tel_port = re.search(
+            "((\+|00)33\s?|0)[67](\s?\d{2}){4}", desc_parsed).group(0)
+    except AttributeError:
         tel_port = ""
-
-    # pprint(price)
-    # pprint(kilometrage)
-    # pprint(release_date)
-    # pprint(tel_fixe)
-    # pprint(tel_port)
 
     type_vendeur = {"p": "particulier", "c": "professionnel"}
 
-    car_dict['version']=modele
-    car_dict['price']=int(price)
-    car_dict['kilometrage']=kilometrage
-    car_dict['release_date']=release_date
-    car_dict['tel_fixe']=tel_fixe
-    car_dict['tel_port']=tel_port
-    car_dict['type_vendeur']=type_vendeur[vendeur]
+    car_dict['version'] = modele
+    car_dict['price'] = int(price)
+    car_dict['kilometrage'] = kilometrage
+    car_dict['release_date'] = release_date
+    car_dict['tel_fixe'] = tel_fixe
+    car_dict['tel_port'] = tel_port
+    car_dict['type_vendeur'] = type_vendeur[vendeur]
 
     return car_dict
 
+
 def get_cote_argus(version, year, kilometrage):
-    url = "https://www.lacentrale.fr/cote-auto-renault-zoe-{version}-{year}.html"
+    url = ("https://www.lacentrale.fr/"
+           "cote-auto-renault-zoe-{version}-{year}.html")
     res = session.get(url.format(version=version,
-                                  year=year), headers=headers)
+                                 year=year), headers=headers)
 
     url_cote_perso = "https://www.lacentrale.fr/get_co_prox.php?km={km}"
     res = session.get(url_cote_perso.format(km=kilometrage), headers=headers)
 
     return int(res.json()['cote_perso'])
 
+
 if __name__ == "__main__":
     start_time = time.time()
     work_dir = "C:\\Users\\Mauva\\Documents\\Work\\INFMDI721\\Lesson4"
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    headers = {
+        'User-Agent': ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1)"
+                       " AppleWebKit/537.36 (KHTML, like Gecko)"
+                       " Chrome/39.0.2171.95 Safari/537.36")}
 
-    url = "https://www.leboncoin.fr/voitures/offres/{region}/?q={version}&brd=Renault&mdl=Zoe&f={vendeur}"
     regions = ["ile_de_france", "aquitaine", "provence_alpes_cote_d_azur"]
     # regions = ["ile_de_france"]
     modeles = ["life", "zen", "intens"]
     # modeles = ["life"]
     vendeurs = ["p", "c"]
     # vendeurs = ["p"]
-    car_list_url=[]
-    [get_car_list(r, m , v, car_list_url) for r in regions for m in modeles for v in vendeurs]
+    car_list_url = []
+    [get_car_list(r, m, v, car_list_url)
+     for r in regions for m in modeles for v in vendeurs]
 
-    car_specs=[]
-    car_specs+=[get_car_specs(x[0], x[1], x[2]) for x in car_list_url]
+    car_specs = []
+    car_specs += [get_car_specs(x[0], x[1], x[2]) for x in car_list_url]
     df = pd.DataFrame(car_specs)
 
     # print(df[['version','release_date','kilometrage']])
-    argus_list=[]
+    argus_list = []
     session = requests.Session()
-    argus_list=[get_cote_argus(row.version,row.release_date,row.kilometrage) for row in df.itertuples()]
+    argus_list = [get_cote_argus(
+        row.version, row.release_date, row.kilometrage)
+        for row in df.itertuples()]
 
-    df_argus = pd.DataFrame(argus_list,columns=["cote argus"])
-
-    df=pd.concat([df, df_argus], axis=1, join_axes=[df.index])
+    df_argus = pd.DataFrame(argus_list, columns=["cote argus"])
+    df = pd.concat([df, df_argus], axis=1, join_axes=[df.index])
 
     df['comparaison argus'] = '='
-
-    df.loc[df['price'] > df['cote argus'],'comparaison argus'] = '+'
-    df.loc[df['price'] < df['cote argus'],'comparaison argus'] = '-'
+    df.loc[df['price'] > df['cote argus'], 'comparaison argus'] = '+'
+    df.loc[df['price'] < df['cote argus'], 'comparaison argus'] = '-'
 
     df.to_csv(work_dir + "\\leboncoin_zoe_offer.csv",
               index=False, sep=";")
